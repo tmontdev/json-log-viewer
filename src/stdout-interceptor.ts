444a9d33926019a3c63921cf8aabd39f2c9a2d91
Thales Montero <thales.monteiro@turbi.com.br>
2026-03-11 10:19:44 -0300

    fix: intercepting all calls to stdout write to ensure console writing

--- src/stdout-interceptor.ts
+++ src/stdout-interceptor.ts
@@ -1,10 +1,6 @@
 const originalWrite = process.stdout.write.bind(process.stdout);
 let isIntercepting = false;
 
-function isJsonLog(str: string): boolean {
-  const trimmed = str.trim();
-  return trimmed.startsWith('{') && trimmed.endsWith('}');
-}
 
 (process.stdout as any).write = function (
   chunk: string | Uint8Array,
@@ -16,7 +12,7 @@ function isJsonLog(str: string): boolean {
     return originalWrite.apply(process.stdout, arguments as any);
   }
   
-  if (!isIntercepting && typeof chunk === 'string' && isJsonLog(chunk)) {
+  if (!isIntercepting && typeof chunk === 'string') {
     const stack = new Error().stack || '';
     const isConsole = stack.includes('at Console.');
     
